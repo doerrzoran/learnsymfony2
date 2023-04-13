7cb3296a34f94dda3607e5b5271d540e873a3833
doerrzoran <doerrzoran@gmail.com>
2023-04-13 13:09:37 +0200

    personalization du formulaire

--- src/Form/PersonneType.php
+++ src/Form/PersonneType.php
@@ -2,7 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Hobbie;
 use App\Entity\Personne;
+use App\Entity\Profile;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\ORM\EntityRepository;
+use PhpParser\Parser\Multiple;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,12 +24,22 @@ class PersonneType extends AbstractType
             ->add('age')
             ->add('createdAt')
             ->add('updatedAt')
-            ->add('profile')
-            ->add('hobbies')
+            ->add('profile', EntityType::class, [
+                'expanded' => true,
+                'class' => Profile::class,
+                'multiple' => false
+            ])
+            ->add('hobbies', EntityType::class, [
+                'expanded' => false,
+                'class' => Hobbie::class,
+                'multiple' => true,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('h.designation', 'ASC');
+                },
+            ])
             ->add('job')
 
-            ->add('edited', SubmitType::class)
-        ;
+            ->add('edited', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
