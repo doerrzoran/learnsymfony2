25474722869fae32586f1d61b1fca5eddcac2975
doerrzoran <doerrzoran@gmail.com>
2023-04-27 10:09:45 +0200

    test de mail

---
+++ src/Services/MailerService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerService
+{
+    public function __construct(private MailerInterface $mailer){}
+    public function sendEmail(): void
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('sending')
+            ->html('<p>sending mail is fun</p>');
+
+        $this->mailer->send($email);
+
+        // ...
+    }
+}
