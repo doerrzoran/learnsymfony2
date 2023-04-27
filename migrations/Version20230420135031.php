22d9934a807c12fb46f7dafbe00d8388939a81f2
doerrzoran <doerrzoran@gmail.com>
2023-04-27 10:15:08 +0200

    modification relation profile-personne

---
+++ migrations/Version20230420135031.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230420135031 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personne DROP INDEX UNIQ_FCEC9EFCCFA12B8, ADD INDEX IDX_FCEC9EFCCFA12B8 (profile_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personne DROP INDEX IDX_FCEC9EFCCFA12B8, ADD UNIQUE INDEX UNIQ_FCEC9EFCCFA12B8 (profile_id)');
+    }
+}
